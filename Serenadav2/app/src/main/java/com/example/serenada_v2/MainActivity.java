909870a7669e98255aa028a8b40633d9cd433304
nocom <nocomdev@bk.ru>
2026-01-28 19:30:41 +0400

    офф экшн бара

--- Serenadav2/app/src/main/java/com/example/serenada_v2/MainActivity.java
+++ Serenadav2/app/src/main/java/com/example/serenada_v2/MainActivity.java
@@ -42,6 +42,9 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
